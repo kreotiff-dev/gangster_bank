be13d22950f2723e0309d61021f63717c4b18cca
Sychev Eugene <dev.stratus@gmail.com>
2024-05-22 16:58:50 +0300

    небольшие правки

--- backend/index.js
+++ backend/index.js
@@ -12,7 +12,10 @@ const errorMiddleware = require('./middlewares/error-middleware')
 // Middleware
 app.use(express.json())
 app.use(cookieParser())
-app.use(cors())
+app.use(cors({
+    credentials: true,
+    origin: process.env.CLIENT_URL
+}))
 app.use('/api', router)
 app.use(errorMiddleware)
 
