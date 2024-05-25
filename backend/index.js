e2742a71c5c58bd112ec4eb934fc3cc29be4b859
Sychev Eugene <dev.stratus@gmail.com>
2024-05-25 23:36:16 +0300

    add logger winston. And add logge to index.js

--- backend/index.js
+++ backend/index.js
@@ -4,6 +4,7 @@ const pool = require('./dbConfig')
 const cors = require('cors')
 const cookieParser = require('cookie-parser')
 const router = require('./routing/index')
+const logger = require('./utils/logger')
 const PORT = process.env.PORT || 3000
 const app = express()
 const errorMiddleware = require('./middlewares/error-middleware')
@@ -27,14 +28,14 @@ const start = async () => {
     try {
         pool.query('SELECT NOW()', (err, res) => {
             if (err) {
-                console.error('Error connecting to db', err)
+                logger.error(`Error connecting to db ${err}`)
             } else {
-                console.log('Current time from database:', res.rows[0].now) //для проверки подключения к бд
-                app.listen(PORT, () => console.log(`Server started on port = ${PORT}`))
+                logger.info(`Current time from database: ${res.rows[0].now}`) //для проверки подключения к бд
+                app.listen(PORT, () => logger.info(`Server started on port = ${PORT}`))
             }
         });
     } catch (e) {
-      console.log('Error starting the server:', e)
+        logger.info(`Error starting the server: ${e}`)
     }
   }
 
