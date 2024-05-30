3e7cc6abdc462c4a18c9567d3116fda5db71a01b
Sychev Eugene <dev.stratus@gmail.com>
2024-05-30 19:24:39 +0300

    добавил изображение карт

--- frontend/src/components/CardList.tsx
+++ frontend/src/components/CardList.tsx
@@ -11,6 +11,9 @@ interface Card {
   cardType: string;
   cardBalance: number;
   currency: string;
+  expirationDate: string;
+  cardholderFirstname: string;
+  cardholderLastname: string;
 }
 
 const currencySymbols: { [key: string]: string } = {
@@ -25,6 +28,30 @@ const currencyPositions: { [key: string]: 'before' | 'after' } = {
   RUB: 'after',
   EUR: 'before',
   // Добавьте другие валюты по мере необходимости
+};
+
+const formatCardNumber = (cardNumber: string) => {
+  return cardNumber.replace(/(\d{4})(?=\d)/g, '$1 ');
+};
+
+const formatExpirationDate = (expirationDate: string) => {
+  const [year, month] = expirationDate.split('-');
+  return `${month}/${year.slice(2)}`;
+};
+
+const getCardImage = (cardType: string) => {
+  switch (cardType) {
+    case 'MasterCard':
+      return '/images/masterCard.png';
+    case 'Visa':
+      return '/images/visa.png';
+    case 'МИР':
+      return '/images/mirCard.png';
+    case 'UnionPay':
+      return '/images/UnionPayCard.png';
+    default:
+      return '/images/defaultCard.png';
+  }
 };
 
 const CardsList: React.FC = () => {
@@ -130,7 +157,12 @@ const CardsList: React.FC = () => {
                 opacity: index === activeIndex ? 1 : 0.5
               }}
             >
-              <h3>{card.cardNumber}</h3>
+              <div className={styles.cardContent}>
+                <img src={getCardImage(card.cardType)} alt={card.cardType} className={styles.cardImage} />
+                <h3 className={styles.cardNumber}>{formatCardNumber(card.cardNumber)}</h3>
+                <div className={styles.cardExpiration}>{formatExpirationDate(card.expirationDate)}</div>
+                <div className={styles.cardholderName}>{card.cardholderFirstname.toUpperCase()} {card.cardholderLastname.toUpperCase()}</div>
+              </div>
               <div className={styles.cardInfo}>
                 <div>Тип: {card.cardType}</div>
                 <div>Баланс: {currencyPositions[card.currency] === 'before' ? `${currencySymbols[card.currency]}${card.cardBalance}` : `${card.cardBalance}${currencySymbols[card.currency]}`}</div>
@@ -160,7 +192,7 @@ const CardsList: React.FC = () => {
       )}
       {showDetails && (
         <div className={styles.cardDetails}>
-          <div>Номер карты: {cards[activeIndex].cardNumber}</div>
+          <div>Номер карты: {formatCardNumber(cards[activeIndex].cardNumber)}</div>
           <div>Тип: {cards[activeIndex].cardType}</div>
           <div>Баланс: {currencyPositions[cards[activeIndex].currency] === 'before' ? `${currencySymbols[cards[activeIndex].currency]}${cards[activeIndex].cardBalance}` : `${cards[activeIndex].cardBalance}${currencySymbols[cards[activeIndex].currency]}`}</div>
         </div>
